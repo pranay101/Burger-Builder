faa640540c689a615977fdbcbe2d302d9c271f3c
pranay101 <pranayprajapati101@gmail.com>
2021-10-19 22:29:09 +0530

    react day 1

--- first/src/App.js
+++ first/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person : [
+      {name:"pranay", age:'21'},
+      {name:"hello", age:'31'},
+      {name:"svsvfv" ,age:'61'}
+    ]
+  }
+
+  switchnameHandler = () =>{
+    console.log("i was clicked!!")
+  }
+  render({
+    return (
+      <div className="App">
+        <h1>Welcome to my react app!!</h1>
+        <button onClick = {this.switchnameHandler}>switch</button>
+        <Person name = {this.state.person[0].name} age = {this.state.person[0].age} />
+        <Person name = {this.state.person[1].name} age = {this.state.person[1].age}> and i like to cycle</Person>
+        <Person name = {this.state.person[2].name} age = {this.state.person[2].age} />
+    
+      </div>
+    );
+  });
+  
 }
 
 export default App;
